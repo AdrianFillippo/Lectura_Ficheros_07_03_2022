c3efa33fd6e159bb30da1fc7a1983cb878fd067a
AdrianFillippo <adrifillip2@gmail.com>
2022-05-03 23:36:58 -0500

    LIsto

--- api.php
+++ api.php
@@ -1,25 +1,8 @@
 <?php
     header("Content-Type: application/json");
-    $data = (object) [];
-    $data->nombre = (string) "Adrian Jesus Ochoa Fillippo";
-    $data->edad = (int) 16;
-    $data->altura = (float) 1.80;
-    $data->estudiante = (bool) true;
-    $data->pasatiempo = (array) [
-        (string) "jugar",
-        (string) "dibujar"
-    ];
-    $data->Animal = (array) [
-        (object) [
-            "Nombre" => (string) "Tita", 
-            "Especie" => (string) "Canino"]
-    ];
-   
+    $datos = file_get_contents("datos.json");
+    $decodificar = json_decode($datos);
 
+    var_dump($decodificar)
 
-    $codificar = json_encode($data, JSON_PRETTY_PRINT);
-    
-    $file = fopen("datos.json", "w+");
-    fwrite($file, $codificar);
-    fclose($file);
 ?>   
